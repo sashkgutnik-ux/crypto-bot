27b77617aaa4963a67f30d3fc6009eaba22bcfe6
sashkgutnik-ux <sashk.gutnik@gmail.com>
2026-03-17 03:03:01 +0100

    Update trading_strategies.py

--- trading_strategies.py
+++ trading_strategies.py
@@ -1,25 +1,18 @@
-class PaperTrader:
-    def __init__(self):
-        self.balance = 1000.0
-        self.btc = 0.0
+def ema_strategy(price):
+    return "BUY" if price % 2 == 0 else "SELL"
 
-    def buy(self, price, amount_usdt):
-        if self.balance >= amount_usdt:
-            btc_bought = amount_usdt / price
-            self.btc += btc_bought
-            self.balance -= amount_usdt
-            print(f"[BUY] BTC: {btc_bought:.6f} по {price}")
-        else:
-            print("Недостаточно USDT")
 
-    def sell(self, price):
-        if self.btc > 0:
-            usdt = self.btc * price
-            print(f"[SELL] BTC: {self.btc:.6f} по {price}")
-            self.balance += usdt
-            self.btc = 0
-        else:
-            print("Нет BTC")
+def rsi_strategy(price):
+    return "BUY" if price < 75000 else "SELL"
 
-    def status(self):
-        print(f"USDT: {self.balance:.2f} | BTC: {self.btc:.6f}")
+
+def breakout_strategy(price):
+    return "BUY" if price > 75500 else "SELL"
+
+
+def bollinger_strategy(price):
+    return "BUY" if price < 75200 else "SELL"
+
+
+def grid_strategy(price):
+    return "HOLD"
