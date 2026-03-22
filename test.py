33e6430abc5254184a31177b762396ddee9b60ef
sashkgutnik-ux <sashk.gutnik@gmail.com>
2026-03-22 02:47:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 from playwright.sync_api import sync_playwright
 
+
 def get_bybit_price():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True, args=["--no-sandbox"])
-        
+        browser = p.chromium.launch(
+            headless=True,
+            args=[
+                "--no-sandbox",
+                "--disable-blink-features=AutomationControlled"
+            ]
+        )
 
-        page = browser.new_page()
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+            locale="en-US"
+        )
+
+        page = context.new_page()
 
         page.goto("https://www.bybit.com/fiat/trade/otc/buy/USDT/EUR", timeout=60000)
 
-        page.wait_for_selector("table", timeout=20000)
+        page.wait_for_timeout(5000)
 
         rows = page.query_selector_all("tr")
 
